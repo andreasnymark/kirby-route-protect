55ee489a5c4505edee2c8025f8dff03d0e06868b
andreasnymark <andreas@nymark.me>
2023-03-11 22:12:55 +0100

    Added comments

--- route-protect.php
+++ route-protect.php
@@ -1,15 +1,18 @@
 <?php
 
-$user_protected = option( 'andreasnymark.kirby-route-protect.slugs' );
+$userProtected = option( 'andreasnymark.kirby-route-protect.slugs' );
 $protected = [];
 $check = '';
 
-if ( gettype( $user_protected ) != 'array' ) throw new Exception( 'Protected route names needs to be in an array.' );
+// make sure user provides slugs as array
+if ( gettype( $userProtected ) != 'array' ) throw new Exception( 'Protected route names needs to be in an array.' );
 
+// setup check var depending on hook event
 switch ( $event ) {
 	case 'page.changeSlug:before' :
 		$parts = explode( '/', $page );
 		if ( count( $parts ) > 1 ) {
+			// remove last word, adding new slug
 			array_pop( $parts );
 			$check = implode( '/', $parts ) . $slug;
 		} else {
@@ -21,16 +24,22 @@ switch ( $event ) {
 		break;
 }
 
+// loop through all existing routes, pick
+// first word and add to array
 foreach ( kirby()->routes() as $route ) {
 	$pattern = $route[ 'pattern' ];
-	$pattern_array = array_values( array_filter( explode( '/', $pattern ), 'strlen' ) );
+	$patternArray = array_values( array_filter( explode( '/', $pattern ), 'strlen' ) );
 
-	if ( count( $pattern_array ) > 0 ) {
-		$protected[] = $pattern_array[ 0 ];
+	// avoiding empty arrays
+	if ( count( $patternArray ) > 0 ) {
+		$protected[] = $patternArray[ 0 ];
 	}
 }
+// merge, keep unique, and remove empty
+$protected = array_values( array_unique( array_merge( $userProtected, $protected ) ) );
 
-$protected = array_values( array_unique( array_merge( $user_protected, $protected ) ) );
+// loop through all routes + user protected
+// and throw error if it’s a match
 foreach ( $protected as $item ) {
 	if ( $check == $item ) {
 		throw new Exception( 'The URL-appendix is protected.' );
